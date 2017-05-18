9bd39975bca1829add2b884f9df5c7a30eff71fb
Laurent Berteau <laurent@berteau.name>
2017-05-18 16:45:15 +0200

    Adding backup script

---
+++ run-rsync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function run-rsync {
+  echo "Running backup from $1 to $2"
+  mkdir -p "$2"
+  rsync --archive --delete-after "$1" "$2"
+  echo "Backup from $1 to $2 successfull"
+}
+
+source="/mnt/d/Photos/"
+target="/mnt/e/Backup/Photos"
+run-rsync ${source} ${target}
+
+source="/mnt/d/Videos/"
+target="/mnt/e/Backup/Videos/"
+run-rsync ${source} ${target}
+
+source="/mnt/d/Archives/"
+target="/mnt/e/Backup/Archives"
+run-rsync ${source} ${target}
+
+source="/mnt/d/Users/"
+target="/mnt/e/Backup/Users"
+run-rsync ${source} ${target}
+
